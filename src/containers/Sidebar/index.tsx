1b8f048274a8d3d822505b09849e31823dde5f06
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-08-14 17:00:40 -0300

    construindo a estrutura e a secao "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/RodrigoFCC.png" />
+    <Titulo fontSize={20}>Rodrigo Carvalho</Titulo>
+  </aside>
+)
+
+export default Sidebar
