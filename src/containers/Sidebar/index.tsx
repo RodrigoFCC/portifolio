1cde6cf87e1c43e63d235075a8fd844bfa778996
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-08-15 16:53:09 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/RodrigoFCC.png" />
-    <Titulo fontSize={20}>Rodrigo Carvalho</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rodrigo Carvalho</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        RodrigoFCC
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Developer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
